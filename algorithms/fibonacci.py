774756bbb821c803d93ac478b8c307497a26ee60
unknown <alba.hokka@gmail.com>
2023-05-13 01:25:13 +0300

    #add not recursion func

--- algorithms/fibonacci.py
+++ algorithms/fibonacci.py
@@ -1,3 +1,4 @@
+from array import array
 """Create algorithm Fibonacci"""
 
 n = int(input("Введи 'n'\n"))
@@ -5,7 +6,7 @@ n = int(input("Введи 'n'\n"))
 def fib_naive(n):
     """
     Function for calculation num fibonacci.
-    This func is not good for elapsed time
+    This func is not good for elapsed time (recursion)
     """
     if n <= 1:
         return n
@@ -13,3 +14,20 @@ def fib_naive(n):
         return fib_naive(n - 1) + fib_naive(n - 2)
 
 print(fib_naive(n))
+
+def fib_effective(n):
+    """
+    This func is fast
+    """
+    f = []
+    f.insert(0, 0)
+    f.insert(1, 1)
+    f.insert(2, 1)
+    if n <= 1:
+        return n
+    else:
+        for i in range(2, n):
+            f[1], f[2] = f[2], f[1] + f[2]
+        return f[2]
+
+print(fib_effective(n))
